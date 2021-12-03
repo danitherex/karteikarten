39ad1f6954ab08c8c5211fb86eb35258f3b90006
cherd31 <95433030+cherd31@users.noreply.github.com>
2021-12-03 18:12:10 +0100

    keineLeerSeiteMehr4

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+
+import { render } from "react-dom";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
