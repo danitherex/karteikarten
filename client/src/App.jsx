7baf8cc9848babddca7b0f9fd94e6a5c38554a97
cherd31 <95433030+cherd31@users.noreply.github.com>
2021-12-03 19:52:19 +0100

    schöner1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,15 +14,25 @@ function App() {
 
   return (
     <div>
-      <h1>Bookkeeper</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem"
-        }}
-      >
-        <Link to="/hi">hi</Link> |{" "}
-        <Link to="/hallo">hallo</Link>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">Quiz</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hi">hi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/hallo">hallo</a>
+            </li>
+          </ul>
+        </div>
       </nav>
       <Outlet />
     </div>
