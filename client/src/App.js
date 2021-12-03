fdc703ba685707ccbff816240d56d60603b86e5e
cherd31 <95433030+cherd31@users.noreply.github.com>
2021-12-03 18:41:54 +0100

    leerWeg1

--- client/src/App.js
+++ client/src/App.js
@@ -15,27 +15,19 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-          </ul>
-        </div>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
       </nav>
       <Outlet />
     </div>
-
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,16 @@ import {
   Route
 } from "react-router-dom";
 import App from "./App";
-import hallo from "./seiten/hallo";
-import hi from "./seiten/hallo";
+import Hallo from "./seiten/hallo";
+import Hi from "./seiten/hallo";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="hallo" element={<hi />} />
-        <Route path="hi" element={<hallo />} />
+        <Route path="hallo" element={<Hi />} />
+        <Route path="hi" element={<Hallo />} />
       </Route>
     </Routes>
   </BrowserRouter>,
