19278d3550e539ffce080e624c3a1fb704b51fb7
Daniel Maurer <daniel@maurer-koblenz.de>
2021-12-03 16:39:39 +0100

    Fixed broken input tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [data, setData] = React.useState(null);
