d2b25cd9b3a0b6f0c73ca0699baa77673546af48
danitherex <daniel@maurer-koblenz.de>
2021-12-03 20:33:19 +0100

    added Title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
 import { Outlet, Link } from "react-router-dom";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Helmet } from 'react-helmet';
 
+const TITLE = 'Karteikarten';
 
-function App() {
-  const [data, setData] = React.useState(null);
+class MyComponent extends React.PureComponent {
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Quiz</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/hi">hi</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/hallo">hallo</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <Outlet />
-    </div>
-  );
+  render() {
+    return (
+      <>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">Quiz</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="/hi">hi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/hallo">hallo</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Outlet />
+      </div>
+      </>
+    );
+  }
 }
-
-export default App;
+export default MyComponent;
