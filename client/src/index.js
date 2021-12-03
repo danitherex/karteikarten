286680ad881e55c3405a6ea17705874adb014cbf
cherd31 <95433030+cherd31@users.noreply.github.com>
2021-12-03 19:43:24 +0100

    pls

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import App from "./App";
+import Hallo from "./seiten/hallo";
+import Hi from "./seiten/hi";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="hallo" element={<Hallo />} />
+        <Route path="hi" element={<Hi />} />
+      </Route>
+    </Routes>
   </BrowserRouter>,
   rootElement
 );
