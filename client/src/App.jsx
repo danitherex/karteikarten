9da26708517401665329516851608a2a08c8627c
danitherex <daniel@maurer-koblenz.de>
2021-12-04 18:03:46 +0100

    sd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button,Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 import axios from 'axios';
 
@@ -20,23 +20,24 @@ class App extends React.Component {
 
     return (
       <>
-        <Helmet>
-          <title>{TITLE}</title>
-        </Helmet>
         <Form>
-          <Form.Group className="mb-3" controlId="vorderseite">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="vorderseite" placeholder="Vorderseite Karteikarten" />
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="rückseite">
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="rückseite" placeholder="Rückseite Karteikarten" />
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Nächste Karte
-          </Button>
-          <Button variant="primary" type="submit">
-            Lernen
+            Submit
           </Button>
         </Form>
       </>
