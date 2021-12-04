8dcfcff036f3a2f16b62d5a8a30bf52de6734b50
danitherex <daniel@maurer-koblenz.de>
2021-12-04 15:48:51 +0100

    axios karteikartenapitest

--- client/src/Quizz.jsx
+++ client/src/Quizz.jsx
@@ -2,13 +2,23 @@ import { Outlet, Link } from "react-router-dom";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Helmet } from 'react-helmet';
+import axios from 'axios';
 
 const TITLE = 'Karteikarten';
 const API = 'https://lernenmitkarteikarten.herokuapp.com/api/items';
 
 class Quizz extends React.PureComponent {
+  state = {
+    karten: []
+  }
 
-
+  componentDidMount() {
+    axios.get(API)
+      .then(res => {
+        const karten = res.data;
+        this.setState({ karten });
+      })
+  }
 
   render() {
     return (
@@ -46,6 +56,9 @@ class Quizz extends React.PureComponent {
             <button type="submit" class="btn btn-primary">Login</button>
           </form>
 
+          <ul>
+            {this.state.persons.map(person => <li>{person.name}</li>)}
+          </ul>
           <Outlet />
         </div>
       </>
