8eacef084230c2c0a23a80da196d7e336e2cd97b
Berni <bernhardbu02@gmailcom>
2021-12-05 13:24:05 +0100

    testbla

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,7 @@ class App extends React.Component {
     }
     return (
       <>
-      <h1>{hits.map()}</h1>
+      <h1>{hits._id.map()}</h1>
         <div>
           <Helmet>
             <title>{TITLE}</title>
