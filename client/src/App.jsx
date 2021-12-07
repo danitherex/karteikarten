961d709a2130bd7f62fc76c7972f1f3a1a16ad61
danitherex <daniel@maurer-koblenz.de>
2021-12-07 15:17:06 +0100

    afw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,8 @@ class App extends React.Component {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
       const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=15c46e32275c804eef0433e4af545129`
-      setTimeout(()=>axios.get(URL)
+
+      setInterval(() => axios.get(URL)
         .then(res => {
           const weatherdata = res.data;
           var date = new Date(weatherdata.sys.sunset * 1000);
@@ -37,7 +38,7 @@ class App extends React.Component {
           console.log("Weather: " + weatherdata.weather[0].description);
           console.log("Sunset at " + formattedTime);
           console.log("Temperature: " + (weatherdata.main.temp - 273.15).toFixed(2) + "°C");
-        }),6000);
+        }), 10000);
     });
   }
   postkarte() {
